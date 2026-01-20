0399a3b5895804414be7ba589a5016de3801abd1
Marisol437 <mvc437@gmail.com>
2026-01-20 14:08:24 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import Home from "./pages/Home.jsx";
 import Test from "./pages/Test.jsx";
+import Result from "./pages/Result.jsx";
 
 function App() {
   const [screen, setScreen] = useState("home");
 
   if (screen === "test") {
-    return <Test />;
+    return <Test onFinish={() => setScreen("result")} />;
+  }
+
+  if (screen === "result") {
+    return <Result onRestart={() => setScreen("home")} />;
   }
 
   return <Home onStart={() => setScreen("test")} />;
