fd12b27402eb0b0ae91f11195cae164811965eb4
Marisol437 <mvc437@gmail.com>
2026-01-20 13:48:47 -0500

    Add files via upload

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,1 +1,23 @@
+function Test() {
+  return (
+    <main>
+      <h1>Diagnóstico de nivel</h1>
+      <p>Pregunta 1 de 20</p>
 
+      <div>
+        <p><strong>Aquí irá la pregunta.</strong></p>
+
+        <ul>
+          <li>Opción A</li>
+          <li>Opción B</li>
+          <li>Opción C</li>
+          <li>Opción D</li>
+        </ul>
+      </div>
+
+      <button type="button">Siguiente</button>
+    </main>
+  );
+}
+
+export default Test;
